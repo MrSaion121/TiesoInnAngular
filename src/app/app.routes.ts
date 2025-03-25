cd46da6a8d82d1a6c5fc79f8bd1757b0c7697a87
Santiago Oseguera <142419366+MrSaion121@users.noreply.github.com>
2025-03-25 17:03:09 -0600

    Carga inicial de rutas y componentes vacios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { ForgotPasswordComponent } from './components/pages/login/forgot-password/forgot-password.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { RoomsComponent } from './components/pages/rooms/rooms.component';
+import { ReservationComponent } from './components/pages/rooms/reservation/reservation.component';
+import { ChatComponent } from './components/pages/chat/chat.component';
+import { ProfileComponent } from './components/pages/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'login', component: LoginComponent, children: [
+        {path: 'forgot-password', component: ForgotPasswordComponent}
+    ] },
+    {path: 'register', component: RegisterComponent},
+    {path: 'rooms', component: RoomsComponent, children: [
+        {path: ':id', component: ReservationComponent}
+    ]},
+    {path: 'chat', component: ChatComponent},
+    {path: 'profile', component: ProfileComponent},
+    
+];
