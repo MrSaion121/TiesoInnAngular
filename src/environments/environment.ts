5fd6fa5175709419256d9021a779488abd500496
Santiago Oseguera <142419366+MrSaion121@users.noreply.github.com>
2025-05-02 15:13:11 -0600

    Carga del servicio de auth

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    googleClientId: '575754777093-gfeoceeiea31nl51meq4a90hbrauh7dk.apps.googleusercontent.com',
-    apiUrl: 'http://localhost:3000'
-};
+    production: false,
+    apiUrl: 'http://localhost:10000/api',
+    googleClientId: '575754777093-gfeoceeiea31nl51meq4a90hbrauh7dk.apps.googleusercontent.com' 
+  };
