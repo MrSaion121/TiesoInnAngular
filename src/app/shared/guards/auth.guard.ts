a32b84b39c9c7e500ccdef543a517f96c2981256
Santiago Oseguera <142419366+MrSaion121@users.noreply.github.com>
2025-05-02 19:22:11 -0600

    Creacion de guardia para autenticacion

---
+++ src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { catchError, map, of } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const httpClient = inject(HttpClient);
+
+  const token = localStorage.getItem('token')
+  if(!token) {
+    router.navigate(['/login']);
+    return of(false);
+  }
+
+  const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+  return httpClient.post<{ valid: boolean }>(`${environment.apiUrl}/me`, { headers })
+  .pipe(
+    map(() => true),
+    catchError(() => {
+      router.navigate(['/login']);
+      return of(false);
+    })
+  )
+};
