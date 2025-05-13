048f9422bc00b95c9e544cafbcc5190db605e554
axelleonardo-1 <123020940+axelleonardo-1@users.noreply.github.com>
2025-05-13 14:32:27 -0600

    Changes on enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+    production: true,
     apiUrl: 'http://localhost:10000/api',
     socketUrl: 'http://localhost:10000',
     googleClientId: '575754777093-gfeoceeiea31nl51meq4a90hbrauh7dk.apps.googleusercontent.com' 
