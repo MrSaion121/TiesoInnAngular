e7cf83e00535a293f8d192068b85d1883854a0af
Santiago Oseguera <142419366+MrSaion121@users.noreply.github.com>
2025-05-14 09:58:24 -0600

    Cambio de URL en environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     googleClientId: '575754777093-gfeoceeiea31nl51meq4a90hbrauh7dk.apps.googleusercontent.com',
-    apiUrl: 'http://localhost:10000/api',
-    socketUrl: 'http://localhost:10000'
+    apiUrl: 'https://tiesoinnapi.onrender.com/api',
+    socketUrl: 'https://tiesoinnapi.onrender.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:10000/api',
+    apiUrl: 'https://tiesoinnapi.onrender.com/api',
     socketUrl: 'https://tiesoinnapi.onrender.com',
     googleClientId: '575754777093-gfeoceeiea31nl51meq4a90hbrauh7dk.apps.googleusercontent.com' 
   };
