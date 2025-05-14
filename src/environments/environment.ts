3bdbfbcbe494476a8ea0e795b95ab49fdd8f2a0e
Santiago Oseguera <142419366+MrSaion121@users.noreply.github.com>
2025-05-14 09:37:09 -0600

    Correccion de socketUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'http://localhost:10000/api',
-    socketUrl: 'http://localhost:10000',
+    socketUrl: 'https://tiesoinnapi.onrender.com',
     googleClientId: '575754777093-gfeoceeiea31nl51meq4a90hbrauh7dk.apps.googleusercontent.com' 
   };
